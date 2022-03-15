a37c6e37ab9511723e5ebeb0ae8d66beae653c22
arturito1994 <artur7632@gmail.com>
2022-03-16 00:10:26 +0100

    dodanie employye detait

--- src/app/pages/employee-detail.page.ts
+++ src/app/pages/employee-detail.page.ts
@@ -1,5 +1,13 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
-@Component({ selector: 'lib-employee-detail-page', templateUrl: './employee-detail.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({ 
+    selector: 'lib-employee-detail-page', 
+    templateUrl: './employee-detail.page.html', 
+    encapsulation: ViewEncapsulation.None, 
+    changeDetection: ChangeDetectionStrategy.OnPush
+ })
 export class EmployeeDetailPage {
+    params$ = this._activatedRoute.params
+    constructor(private _activatedRoute: ActivatedRoute) {}
 }
