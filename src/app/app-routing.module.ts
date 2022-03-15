7cf85756f87db23d97c31c5ec7b03541f9493ef3
arturito1994 <artur7632@gmail.com>
2022-03-15 23:11:44 +0100

    hobbici dynimiczni

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageModule } from './pages/home.page-module';
 import { AboutUsPageModule } from './pages/about-us.page-module';
 import { PracownicyPageModule } from './pages/pracownicy.page-module';
+import { EmployeeDetailPageModule } from './pages/employee-detail.page-module';
 
 const routes: Routes = [{ 
         path: 'home', 
@@ -15,6 +16,10 @@ const routes: Routes = [{
   { 
         path: 'hobbici', 
         loadChildren: () => PracownicyPageModule
+      },
+  { 
+        path: 'hobbici/:name', 
+        loadChildren: () => EmployeeDetailPageModule
       }
 ];
 
