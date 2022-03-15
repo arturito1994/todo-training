4b635a86225694d600426e5e8e2951f2fc602c10
arturito1994 <artur7632@gmail.com>
2022-03-15 22:28:13 +0100

    s

---
+++ src/app/pages/employee-detail.page.ts
@@ -0,0 +1,5 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({ selector: 'lib-employee-detail-page', templateUrl: './employee-detail.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+export class EmployeeDetailPage {
+}
