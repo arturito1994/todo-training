73a12cf78da29433c1da8eb3ab00dddf186afe0b
arturito1994 <artur7632@gmail.com>
2022-03-16 00:38:08 +0100

    pierwszy smart component

---
+++ projects/hobbici/src/lib/adapters/primary/ui/hobbits-list.component.ts
@@ -0,0 +1,5 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({ selector: 'lib-hobbits-list', templateUrl: './hobbits-list.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+export class HobbitsListComponent {
+}
--- projects/hobbici/src/public-api.ts
+++ projects/hobbici/src/public-api.ts
@@ -2,4 +2,4 @@
  * Public API Surface of hobbici
  */
 
-
+export * from './lib/adapters/primary/ui/hobbits-list.component-module';
