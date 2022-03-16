9fc0fbd040f273b8699b95ff6eaa23b2960ffa16
arturito1994 <artur7632@gmail.com>
2022-03-17 00:55:25 +0100

    adres i imie

--- projects/hobbici/src/lib/adapters/primary/ui/kontakt-list.component.ts
+++ projects/hobbici/src/lib/adapters/primary/ui/kontakt-list.component.ts
@@ -3,10 +3,12 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({ selector: 'lib-kontakt-list', templateUrl: './kontakt-list.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class KontaktListComponent {
-    autor="Wpisz imie i kliknij sumbit";
+    autor="Wpisz imie i kliknij sumbit"
+    autor1="Wpisz adres i kliknij sumbit";
   readonly kontakt: FormGroup = new FormGroup({imie: new FormControl(), adres: new FormControl()});
 
     submit(kontakt:FormGroup){
-        this.autor=kontakt.get("imie").value;
+        this.autor=kontakt.get("imie").value
+        this.autor1=kontakt.get("adres").value;
     }
 }
