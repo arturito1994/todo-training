8ecf049b19426ffd50cbbc43346dcfd51766442f
arturito1994 <artur7632@gmail.com>
2022-03-20 19:49:22 +0100

    sub dto

--- projects/hobbici/src/lib/adapters/primary/ui/hobbits-list.component.ts
+++ projects/hobbici/src/lib/adapters/primary/ui/hobbits-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HobbitsDTO } from '../../../application/ports/secondary/hobbits.dto';
+import { DepartmentDTO } from '../../../application/ports/secondary/department.dto';
 
 @Component({ selector: 'lib-hobbits-list', templateUrl: './hobbits-list.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class HobbitsListComponent {
@@ -9,19 +10,31 @@ export class HobbitsListComponent {
            id:"1",
            name: "Frodo",
            bio: 'Tutaj opis Froda',
-           imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS20MsuUVAVx_n8_-N9Y_xM7X_0LJhqoa3n7WDztBbzACpVO4MH_eSlyY3YQOBa51xrOnY&usqp=CAU'
+           imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS20MsuUVAVx_n8_-N9Y_xM7X_0LJhqoa3n7WDztBbzACpVO4MH_eSlyY3YQOBa51xrOnY&usqp=CAU',
+           department:{
+              name:'Baggins',
+              empoyeeCount:1,
+           },
         },
         {
             id:"2",
             name: "Sam",
             bio: 'Tutaj opis Sama',
-            imageUrl: 'https://fwcdn.pl/fph/10/65/1065/447278.2.jpg'
+            imageUrl: 'https://fwcdn.pl/fph/10/65/1065/447278.2.jpg',
+            department:{
+               name:'Gamgee',
+               empoyeeCount:2,
+            },
          },
          {
             id:"3",
             name: "Bilbo",
             bio: 'Tutaj Opis Bilba',
-            imageUrl: 'https://img.wprost.pl/img/ian-holm-jako-bilbo-baggins-w-filmie-wladca-pierscieni-druzyna-pierscienia/3c/35/188fd402e7c93ca560ba01742239.png'
+            imageUrl: 'https://img.wprost.pl/img/ian-holm-jako-bilbo-baggins-w-filmie-wladca-pierscieni-druzyna-pierscienia/3c/35/188fd402e7c93ca560ba01742239.png',
+            department:{
+               name:'Baggins',
+               empoyeeCount:3,
+            },
          },
     ])
 }
--- projects/hobbici/src/lib/application/ports/secondary/hobbits.dto.ts
+++ projects/hobbici/src/lib/application/ports/secondary/hobbits.dto.ts
@@ -1,6 +1,8 @@
+import { DepartmentDTO } from "./department.dto";
 export interface HobbitsDTO {
   readonly id: string;
   readonly name: string;
   readonly imageUrl: string;
   readonly bio: string;
+  readonly department: DepartmentDTO;
 }
