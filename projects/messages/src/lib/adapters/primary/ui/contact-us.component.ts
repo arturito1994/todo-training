93c8fe079300d70107d10b07256414399a8d1675
arturito1994 <artur7632@gmail.com>
2022-03-29 00:08:06 +0200

    .

---
+++ projects/messages/src/lib/adapters/primary/ui/contact-us.component.ts
@@ -0,0 +1,5 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({ selector: 'lib-contact-us', templateUrl: './contact-us.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+export class ContactUsComponent {
+}
